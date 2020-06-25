d69c5773c7ac1ae95d2cdb6408958406fa16f2c4
Zach Saw <zach.saw@carsales.com.au>
2020-06-25 12:24:02 +1000

    Fix parameterless constructor error

--- src/Binaron.Serializer.Tests/ValueSerializationTests.cs
+++ src/Binaron.Serializer.Tests/ValueSerializationTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Binaron.Serializer.CustomObject;
 using Binaron.Serializer.Tests.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
 namespace Binaron.Serializer.Tests
@@ -12,66 +14,66 @@ namespace Binaron.Serializer.Tests
         public void RootLevelValueTests<TDestination>(object source, TDestination expectation)
         {
             var (dest, dest2) = Tester.TestRoundTrip2<TDestination>(source);
-
+        
             Assert.AreEqual(expectation, dest);
             Assert.AreEqual(GetExpectation(source), dest2);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelValueInClassTests<TSource, TDestination>(TSource source, TDestination expectation)
         {
             var now = DateTime.UtcNow;
             var sourceClass = new TestClass<TSource> {RootValue = now, Value = source};
             (var destClass, dynamic dest) = Tester.TestRoundTrip2<TestClass<TDestination>>(sourceClass);
-
+        
             Assert.AreEqual(now, destClass.RootValue);
             Assert.AreEqual(expectation, destClass.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(GetExpectation(source), dest.Value);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelValueInStructTests<TSource, TDestination>(TSource source, TDestination expectation)
         {
             var now = DateTime.UtcNow;
             var sourceStruct = new TestStruct<TSource> {RootValue = now, Value = source};
             (var destStruct, dynamic dest) = Tester.TestRoundTrip2<TestStruct<TDestination>>(sourceStruct);
-
+        
             Assert.AreEqual(now, destStruct.RootValue);
             Assert.AreEqual(expectation, destStruct.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(GetExpectation(source), dest.Value);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void RootLevelValueTests(object source, object _)
         {
             var (dest, dest2) = Tester.TestRoundTrip2<object>(source);
-
+        
             Assert.AreEqual(GetExpectation(source), dest);
             Assert.AreEqual(GetExpectation(source), dest2);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelValueInClassTests<TSource>(TSource source, object _)
         {
             var now = DateTime.UtcNow;
             var sourceClass = new TestClass<TSource> {RootValue = now, Value = source};
             (var destClass, dynamic dest) = Tester.TestRoundTrip2<TestClass<object>>(sourceClass);
-
+        
             Assert.AreEqual(now, destClass.RootValue);
             Assert.AreEqual(GetExpectation(source), destClass.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(GetExpectation(source), dest.Value);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelValueInStructTests<TSource>(TSource source, object _)
         {
             var now = DateTime.UtcNow;
             var sourceStruct = new TestStruct<TSource> {RootValue = now, Value = source};
             (var destStruct, dynamic dest) = Tester.TestRoundTrip2<TestStruct<object>>(sourceStruct);
-
+        
             Assert.AreEqual(now, destStruct.RootValue);
             Assert.AreEqual(GetExpectation(source), destStruct.Value);
             Assert.AreEqual(now, dest.RootValue);
@@ -82,90 +84,90 @@ namespace Binaron.Serializer.Tests
         public void RootLevelNullToValueTests<TDestination>(object _, TDestination __)
         {
             var (dest, dest2) = Tester.TestRoundTrip2<TDestination>(null);
-
+        
             Assert.AreEqual(default(TDestination), dest);
             Assert.AreEqual(null, dest2);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelNullToValueInClassTests<TDestination>(object _, TDestination __)
         {
             var now = DateTime.UtcNow;
             var sourceClass = new TestClass<object> {RootValue = now, Value = null};
             (var destClass, dynamic dest) = Tester.TestRoundTrip2<TestClass<TDestination>>(sourceClass);
-
+        
             Assert.AreEqual(now, destClass.RootValue);
             Assert.AreEqual(default(TDestination), destClass.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(null, dest.Value);
         }
-
+        
         [TestCaseSource(typeof(AllTestCases), nameof(AllTestCases.TestCases))]
         public void NonRootLevelNullToValueInStructTests<TDestination>(object _, TDestination __)
         {
             var now = DateTime.UtcNow;
             var sourceStruct = new TestStruct<object> {RootValue = now, Value = null};
             (var destStruct, dynamic dest) = Tester.TestRoundTrip2<TestStruct<TDestination>>(sourceStruct);
-
+        
             Assert.AreEqual(now, destStruct.RootValue);
             Assert.AreEqual(default(TDestination), destStruct.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(null, dest.Value);
         }
-
+        
         [Test]
         public void NestedStructInClassAsObjectTest()
         {
             var sourceClass = new TestClass<object> {Value = new TestStruct<object> {Value = 1}};
             var dest = Tester.TestRoundTrip<TestClass<TestStruct<object>>>(sourceClass);
-
+        
             Assert.AreEqual(1, dest.Value.Value);
         }
-
+        
         [Test]
         public void NestedStructInClassAsTypedObjectTest()
         {
             var sourceClass = new TestClass<TestStruct<object>> {Value = new TestStruct<object> {Value = 1}};
             var dest = Tester.TestRoundTrip(sourceClass);
-
+        
             Assert.AreEqual(1, dest.Value.Value);
         }
-
+        
         [Test]
         public void RootLevelNullToObjectTest()
         {
             var (dest, dest2) = Tester.TestRoundTrip2<object>(null);
-
+        
             Assert.AreEqual(null, dest);
             Assert.AreEqual(null, dest2);
         }
-
+        
         [Test]
         public void NonRootLevelNullToObjectInClassTest()
         {
             var now = DateTime.UtcNow;
             var sourceClass = new TestClass<object> {RootValue = now, Value = null};
             (var destClass, dynamic dest) = Tester.TestRoundTrip2(sourceClass);
-
+        
             Assert.AreEqual(now, destClass.RootValue);
             Assert.AreEqual(null, destClass.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(null, dest.Value);
         }
-
+        
         [Test]
         public void NonRootLevelNullToObjectInStructTest()
         {
             var now = DateTime.UtcNow;
             var sourceStruct = new TestStruct<object> {RootValue = now, Value = null};
             (var destStruct, dynamic dest) = Tester.TestRoundTrip2(sourceStruct);
-
+        
             Assert.AreEqual(now, destStruct.RootValue);
             Assert.AreEqual(null, destStruct.Value);
             Assert.AreEqual(now, dest.RootValue);
             Assert.AreEqual(null, dest.Value);
         }
-
+        
         [Test]
         public void ClassToStructTest()
         {
@@ -173,7 +175,7 @@ namespace Binaron.Serializer.Tests
             var dest = Tester.TestRoundTrip<TestStruct<int>>(val);
             Assert.AreEqual(1, dest.Value);
         }
-
+        
         [Test]
         public void StructToClassTest()
         {
@@ -221,18 +223,104 @@ namespace Binaron.Serializer.Tests
             var dest = Tester.TestRoundTrip<TestStruct<int?>?>(val);
             Assert.AreEqual(null, dest?.Value);
         }
-
+        
         [Test]
         public void ObjectActivatorTest()
         {
             const int testVal = 100;
-
+        
             var val = new TestClass<int?>();
             using var stream = new MemoryStream();
             BinaronConvert.Serialize(val, stream, new SerializerOptions {SkipNullValues = true});
             stream.Seek(0, SeekOrigin.Begin);
             var dest = BinaronConvert.Deserialize<TestClass<int?>>(stream, new DeserializerOptions {ObjectActivator = new MyObjectActivator(testVal)});
             Assert.AreEqual(testVal, dest.Value);
+        }
+
+        public class BaseConfig
+        {
+            public string Name { get; set; } = "base name";
+        }
+
+        public class TestConfig
+        {
+            public TestConfig(BaseConfig config)
+            {
+                Name = config.Name;
+            }
+
+            public string Name { get; set; }
+            public double Score { get; set; }
+            public int[] Numbers { get; set; }
+            public Dictionary<string, string> Dict { get; set; }
+            public HashSet<string> Ids { get; set; }
+            public SubConfig SubConfig { get; set; }
+        }
+
+        public class SubConfig
+        {
+            public SubConfig(BaseConfig config)
+            {
+                Name = $"sub {config.Name}";
+            }
+
+            public string Name { get; set; }
+            public double Score { get; set; }
+            public int[] Numbers { get; set; }
+            public Dictionary<string, string> Dict { get; set; }
+            public HashSet<string> Ids { get; set; }
+        }
+
+        [Test]
+        public void ServiceProviderObjectActivatorTest()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton(new BaseConfig());
+
+            var obj = new
+            {
+                Score = 1.0d,
+                Numbers = new[] {3, 2, 1},
+                Dict = new
+                {
+                    A = "a",
+                    B = "b"
+                },
+                Ids = new[] {"Id1", "Id2"},
+                SubConfig = new
+                {
+                    Numbers = new[] {3, 2, 1},
+                    Dict = new
+                    {
+                        A = "a",
+                        B = "b"
+                    },
+                    Ids = new[] {"Id1", "Id2"},
+                    Extra = "extra"
+                }
+            };
+
+            var serviceProvider = services.BuildServiceProvider();
+            using var stream = new MemoryStream();
+            BinaronConvert.Serialize(obj, stream, new SerializerOptions {SkipNullValues = true});
+            stream.Position = 0;
+            var result = BinaronConvert.Deserialize<TestConfig>(stream, new DeserializerOptions {ObjectActivator = new ObjectActivator(serviceProvider)});
+            Assert.AreEqual(obj.SubConfig.Ids, result.SubConfig.Ids);
+        }
+
+        private class ObjectActivator : IObjectActivator
+        {
+            private readonly IServiceProvider serviceProvider;
+
+            public ObjectActivator(IServiceProvider serviceProvider)
+            {
+                this.serviceProvider = serviceProvider;
+            }
+
+            public object Create(Type type)
+            {
+                return ActivatorUtilities.CreateInstance(serviceProvider, type);
+            }
         }
 
         [Test]
