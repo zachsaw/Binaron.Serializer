7c2666c1632c48fb915fd360815d90603db41676
Zach Saw <zach.saw@carsales.com.au>
2020-07-20 12:50:51 +1000

    Support IReadOnlyDictionary

--- src/Binaron.Serializer.Tests/DictionarySerializationTests.cs
+++ src/Binaron.Serializer.Tests/DictionarySerializationTests.cs
@@ -24,10 +24,35 @@ namespace Binaron.Serializer.Tests
             Assert.AreEqual(1, dest2.Count());
         }
 
+        [TestCaseSource(nameof(AllListCases))]
+        public void RootReadOnlyDictionaryTests(Type destType)
+        {
+            IReadOnlyDictionary<string, string> source = new Dictionary<string, string> {{"a key", "a value"}};
+            using var stream = new MemoryStream();
+            BinaronConvert.Serialize(source, stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            var dest = (IEnumerable) Converter.Deserialize(destType, stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            var dest2 = (IEnumerable) BinaronConvert.Deserialize(stream);
+            Assert.AreEqual(1, dest.Count());
+            Assert.AreEqual(1, dest2.Count());
+        }
+
         [Test]
         public void ClassWithDictionaryTest()
         {
             var source = new ClassWithDictionary {Dictionary = new Dictionary<string, object> {{"a key", "a value"}}};
+            using var stream = new MemoryStream();
+            BinaronConvert.Serialize(source, stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            dynamic dest = BinaronConvert.Deserialize(stream);
+            Assert.AreEqual("a value", ((IDictionary<string, object>) dest.Dictionary)["a key"]);
+        }
+
+        [Test]
+        public void ClassWithReadOnlyDictionaryTest()
+        {
+            var source = new ClassWithReadOnlyDictionary {Dictionary = new Dictionary<string, object> {{"a key", "a value"}}};
             using var stream = new MemoryStream();
             BinaronConvert.Serialize(source, stream);
             stream.Seek(0, SeekOrigin.Begin);
@@ -41,6 +66,8 @@ namespace Binaron.Serializer.Tests
             const int val = int.MinValue;
             var dest = Tester.TestRoundTrip<IDictionary<int?, int?>>(new Dictionary<int, int>{{val, val}});
             Assert.AreEqual((val, val), (dest.Single().Key, dest.Single().Value));
+            var dest2 = Tester.TestRoundTrip<IReadOnlyDictionary<int?, int?>>(new Dictionary<int, int>{{val, val}});
+            Assert.AreEqual((val, val), (dest2.Single().Key, dest2.Single().Value));
         }
 
         [Test]
@@ -49,6 +76,8 @@ namespace Binaron.Serializer.Tests
             const TestByteEnum val = TestByteEnum.Max;
             var dest = Tester.TestRoundTrip<IDictionary<TestByteEnum?, TestByteEnum?>>(new Dictionary<TestByteEnum, TestByteEnum>{{val, val}});
             Assert.AreEqual((val, val), (dest.Single().Key, dest.Single().Value));
+            var dest2 = Tester.TestRoundTrip<IReadOnlyDictionary<TestByteEnum?, TestByteEnum?>>(new Dictionary<TestByteEnum, TestByteEnum>{{val, val}});
+            Assert.AreEqual((val, val), (dest2.Single().Key, dest2.Single().Value));
         }
         
         [Test]
@@ -60,8 +89,13 @@ namespace Binaron.Serializer.Tests
             Assert.AreEqual(val.NullableValue, dest.Single().Key?.NullableValue);
             Assert.AreEqual(val.Value, dest.Single().Value?.Value);
             Assert.AreEqual(val.NullableValue, dest.Single().Value?.NullableValue);
+            var dest2 = Tester.TestRoundTrip<IReadOnlyDictionary<TestStruct?, TestStruct?>>(new Dictionary<TestStruct, TestStruct>{{val, val}});
+            Assert.AreEqual(val.Value, dest2.Single().Key?.Value);
+            Assert.AreEqual(val.NullableValue, dest2.Single().Key?.NullableValue);
+            Assert.AreEqual(val.Value, dest2.Single().Value?.Value);
+            Assert.AreEqual(val.NullableValue, dest2.Single().Value?.NullableValue);
         }
-
+        
         public struct TestStruct
         {
             public int Value { get; set; }
@@ -75,6 +109,7 @@ namespace Binaron.Serializer.Tests
                 typeof(Dictionary<,>),
                 typeof(IDictionary<,>),
                 typeof(IDictionary),
+                typeof(IReadOnlyDictionary<,>),
                 typeof(ICollection<>),
                 typeof(ICollection),
                 typeof(IEnumerable<>),
@@ -95,12 +130,17 @@ namespace Binaron.Serializer.Tests
             }
         }
         
+        private class ClassWithReadOnlyDictionary
+        {
+            public IReadOnlyDictionary<string, object> Dictionary { get; set; }
+        }
+
         private class ClassWithDictionary
         {
             public Dictionary<string, object> Dictionary { get; set; }
         }
 
-        private class TestDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+        private class TestDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
         {
             private readonly Dictionary<TKey, TValue> backing = new Dictionary<TKey, TValue>();
             
@@ -166,6 +206,10 @@ namespace Binaron.Serializer.Tests
                 get => backing[key];
                 set => backing[key] = value;
             }
+
+            IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => Keys;
+
+            IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => Values;
 
             public ICollection<TKey> Keys => backing.Keys;
             public ICollection<TValue> Values => backing.Values;
